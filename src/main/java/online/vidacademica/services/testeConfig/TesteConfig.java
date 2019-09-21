14f08d712d05a0f4dabb11506bbbec56b7d36549
Carlos <cgletras@gmail.com>
2019-09-21 01:06:54 -0300

    Service layer, USER find by id

--- src/main/java/online/vidacademica/services/testeConfig/TesteConfig.java
+++ src/main/java/online/vidacademica/services/testeConfig/TesteConfig.java
@@ -1,5 +1,6 @@
 package online.vidacademica.services.testeConfig;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +24,10 @@ public class TesteConfig implements CommandLineRunner {
 		
 		// ADICIONA OS ELEMENTOS DO BANCO TESTE
 		
-		User u1 = new User("Carlos Gustavo", null, "A2", "ASD", "123", null);
-		User u2 = new User("Eduardo Augusto", null, "A3", "ABD", "123", null);
-		User u3 = new User("Rafael Sotero", null, "B5", "CSD", "123", null);
-		User u4 = new User("Tiago Marques", null, "C3", "JSD", "123", null);
+		User u1 = new User("Carlos Gustavo", null, "A2", "ASD", "123", Instant.now());
+		User u2 = new User("Eduardo Augusto", null, "A3", "ABD", "123", Instant.now());
+		User u3 = new User("Rafael Sotero", null, "B5", "CSD", "123", Instant.now());
+		User u4 = new User("Tiago Marques", null, "C3", "JSD", "123", Instant.now());
 	
 		userRepository.saveAll(Arrays.asList(u1,u2,u3,u4));
 	
