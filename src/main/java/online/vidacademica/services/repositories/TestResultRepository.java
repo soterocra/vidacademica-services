8312b6d504428342e78cbc079297a5f905fb35f8
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-11-05 20:07:20 -0300

    Add query to search testresult by user_id

--- src/main/java/online/vidacademica/services/repositories/TestResultRepository.java
+++ src/main/java/online/vidacademica/services/repositories/TestResultRepository.java
@@ -1,9 +1,14 @@
 package online.vidacademica.services.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import online.vidacademica.services.entities.TestResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TestResultRepository extends JpaRepository<TestResult, Long> {
+
+    @Query(value = "SELECT * FROM tb_test_result tr WHERE tr.user_id = ?", nativeQuery = true)
+    List<TestResult> findByUserId(Long user_id);
 
 }
