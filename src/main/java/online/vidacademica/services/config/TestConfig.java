20b567dca68162c31a4807cb334d87656a7986ca
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-11-06 19:06:50 -0300

    changed structure of IT tests

--- src/main/java/online/vidacademica/services/config/TestConfig.java
+++ src/main/java/online/vidacademica/services/config/TestConfig.java
@@ -1,6 +1,7 @@
 package online.vidacademica.services.config;
 
-import online.vidacademica.services.entities.*;
+import online.vidacademica.services.entities.Role;
+import online.vidacademica.services.entities.User;
 import online.vidacademica.services.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,11 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.time.*;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 
 @Configuration
-@Profile({"test", "testmariadb"})
+@Profile({"as"})
 public class TestConfig implements CommandLineRunner {
 
     // ADICIONAR REPOSITORIOS
@@ -64,29 +66,6 @@ public class TestConfig implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        // ADICIONA OS ELEMENTOS DO BANCO TESTE
 
-        User u1 = new User(null, "Carlos Gustavo", "cgletras@gmail.com", null, "A2", "ASD", passwordEncode.encode("123"), Instant.now());
-        User u2 = new User(null, "Eduardo Augusto", "dudu@gmail.com", null, "A3", "ABD", passwordEncode.encode("123"), Instant.now());
-        User u3 = new User(null, "Rafael Sotero", "soso@gmail.com", null, "B5", "CSD", passwordEncode.encode("123"), Instant.now());
-        User u4 = new User(null, "Tiago Marques", "titi@gmail.com", null, "C3", "JSD", passwordEncode.encode("123"), Instant.now());
-        User u5 = new User(null, "Nelio Alves", "nelio@gmail.com", null, "d4", "JSD", passwordEncode.encode("123"), Instant.now());
-        User u6 = new User(null, "Cricia", "cricia@gmail.com", null, "d4", "JSD", passwordEncode.encode("123"), Instant.now());
-
-        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
-
-        Role role_Teacher = new Role(null, "ROLE_TEACHER");
-        Role role_Student = new Role(null, "ROLE_STUDENT");
-
-        roleRepository.saveAll(Arrays.asList(role_Teacher, role_Student));
-
-        u1.getRoles().add(role_Student);
-        u2.getRoles().add(role_Student);
-        u3.getRoles().add(role_Student);
-        u4.getRoles().add(role_Student);
-        u5.getRoles().add(role_Teacher);
-        u6.getRoles().add(role_Teacher);
-
-        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
     }
 }
--- src/test/java/online/vidacademica/services/resources/ClassResourceIT.java
+++ src/test/java/online/vidacademica/services/resources/ClassResourceIT.java
@@ -1,56 +1,112 @@
 package online.vidacademica.services.resources;
 
+import online.vidacademica.services.entities.Classe;
+import online.vidacademica.services.entities.Role;
+import online.vidacademica.services.entities.User;
+import online.vidacademica.services.repositories.ClassRepository;
+import online.vidacademica.services.repositories.RoleRepository;
+import online.vidacademica.services.repositories.UserRepository;
 import online.vidacademica.services.security.JWTUtil;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Arrays;
 
-@ActiveProfiles("test")
+//@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ClassResourceIT {
 
     private static final String URI_PATH = "/class";
-    private static final String STUDENT_USER_LOGIN = "soso@gmail.com";
 
+    @Autowired
+    private TestRestTemplate restTemplate;
     @Autowired
     private JWTUtil jwtUtil;
-
     @Autowired
-    private WebApplicationContext context;
+    private UserRepository userRepository;
+    @Autowired
+    private ClassRepository classRepository;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncode;
 
-    private MockMvc mockMvc;
     private HttpHeaders headers = new HttpHeaders();
+    private User teacher;
+    private User student;
+
+    private Classe classe1;
+    private Classe classe2;
 
     @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders
-                .webAppContextSetup(context)
-                .apply(springSecurity())
-                .build();
-
-        String token = jwtUtil.generateToken(STUDENT_USER_LOGIN);
+    public void setUp() {
+        createUsers();
+        createClasses();
+        String token = jwtUtil.generateToken(teacher.getUsername());
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
     }
 
     @Test
     public void findAll_sucess() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get(URI_PATH).accept(MediaType.APPLICATION_JSON_UTF8).headers(headers);
-        mockMvc.perform(request).andExpect(status().isOk());
+
+        HttpEntity<Classe[]> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Classe[]> response = restTemplate.exchange(URI_PATH, HttpMethod.GET, entity, Classe[].class);
+
+        assertThat(response.getBody()).hasSize(2);
+    }
+
+    @Test
+    public void findAll_sucess2() throws Exception {
+
+        HttpEntity<Classe[]> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Classe[]> response = restTemplate.exchange(URI_PATH, HttpMethod.GET, entity, Classe[].class);
+
+        assertThat(response.getBody()).hasSize(2);
+    }
+
+    public void createUsers() {
+        teacher = new User(null, "Teacher 1", "teacher@example.com", Instant.now(), "21234444", "ABD", passwordEncode.encode("123456"), Instant.now());
+        student = new User(null, "Student 1", "student@example.com", Instant.now(), "15309232", "ASD", passwordEncode.encode("123456"), Instant.now());
+
+        userRepository.saveAll(Arrays.asList(teacher, student));
+
+        Role roleTeacher = new Role(null, "ROLE_TEACHER");
+        Role roleStudent = new Role(null, "ROLE_STUDENT");
+
+        roleRepository.saveAll(Arrays.asList(roleTeacher, roleStudent));
+
+        teacher.getRoles().add(roleTeacher);
+        student.getRoles().add(roleStudent);
+
+        userRepository.saveAll(Arrays.asList(teacher, student));
+    }
+
+    public void createClasses() {
+
+        classe1 = new Classe(null, "SI -2018/01", LocalDate.of(2018, 1, 1),
+                LocalDate.of(2019, 7, 22), true, Instant.parse("2018-01-01T00:21:22Z"));
+
+        classe2 = new Classe(null, "LCI - 2018/01", LocalDate.of(2018, 1, 1),
+                LocalDate.of(2019, 7, 22), true, Instant.parse("2018-01-01T00:21:22Z"));
+
+        classRepository.saveAll(Arrays.asList(classe1, classe2));
+
     }
 
 }
