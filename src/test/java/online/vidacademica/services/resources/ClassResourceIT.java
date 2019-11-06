fb5ef738693c06bd84b8836789a799a2039cd6a4
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-11-06 19:38:13 -0300

    Ajusts on setUp method comportament

--- src/test/java/online/vidacademica/services/resources/ClassResourceIT.java
+++ src/test/java/online/vidacademica/services/resources/ClassResourceIT.java
@@ -7,9 +7,6 @@ import online.vidacademica.services.repositories.ClassRepository;
 import online.vidacademica.services.repositories.RoleRepository;
 import online.vidacademica.services.repositories.UserRepository;
 import online.vidacademica.services.security.JWTUtil;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,6 +23,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 //@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -47,24 +46,33 @@ public class ClassResourceIT {
     @Autowired
     private BCryptPasswordEncoder passwordEncode;
 
-    private HttpHeaders headers = new HttpHeaders();
-    private User teacher;
-    private User student;
+    private static HttpHeaders headers = new HttpHeaders();
+    private static User teacher;
+    private static User student;
 
-    private Classe classe1;
-    private Classe classe2;
+    private static Classe classe1;
+    private static Classe classe2;
+
+    private static boolean setUpIsDone = false;
 
     @Before
     public void setUp() {
+
+        if (setUpIsDone) {
+            return;
+        }
+
         createUsers();
         createClasses();
+
         String token = jwtUtil.generateToken(teacher.getUsername());
         headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+
+        setUpIsDone = true;
     }
 
     @Test
     public void findAll_sucess() throws Exception {
-
         HttpEntity<Classe[]> entity = new HttpEntity<>(null, headers);
         ResponseEntity<Classe[]> response = restTemplate.exchange(URI_PATH, HttpMethod.GET, entity, Classe[].class);
 
@@ -73,7 +81,6 @@ public class ClassResourceIT {
 
     @Test
     public void findAll_sucess2() throws Exception {
-
         HttpEntity<Classe[]> entity = new HttpEntity<>(null, headers);
         ResponseEntity<Classe[]> response = restTemplate.exchange(URI_PATH, HttpMethod.GET, entity, Classe[].class);
 
@@ -81,8 +88,10 @@ public class ClassResourceIT {
     }
 
     public void createUsers() {
-        teacher = new User(null, "Teacher 1", "teacher@example.com", Instant.now(), "21234444", "ABD", passwordEncode.encode("123456"), Instant.now());
-        student = new User(null, "Student 1", "student@example.com", Instant.now(), "15309232", "ASD", passwordEncode.encode("123456"), Instant.now());
+        teacher = new User(null, "Teacher 1", "teacher@example.com", Instant.now(),
+                "21234444", "ABD", passwordEncode.encode("123456"), Instant.now());
+        student = new User(null, "Student 1", "student@example.com", Instant.now(),
+                "15309232", "ASD", passwordEncode.encode("123456"), Instant.now());
 
         userRepository.saveAll(Arrays.asList(teacher, student));
 
@@ -98,7 +107,6 @@ public class ClassResourceIT {
     }
 
     public void createClasses() {
-
         classe1 = new Classe(null, "SI -2018/01", LocalDate.of(2018, 1, 1),
                 LocalDate.of(2019, 7, 22), true, Instant.parse("2018-01-01T00:21:22Z"));
 
