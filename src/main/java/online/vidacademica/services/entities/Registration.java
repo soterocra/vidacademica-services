825399c1b780e31477bb473157952363c5c38f58
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-12-10 01:32:06 -0300

    Anotação date para o registration

--- src/main/java/online/vidacademica/services/entities/Registration.java
+++ src/main/java/online/vidacademica/services/entities/Registration.java
@@ -1,5 +1,7 @@
 package online.vidacademica.services.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
@@ -12,6 +14,8 @@ public class Registration implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant date;
 
     @ManyToOne(cascade = CascadeType.MERGE)
